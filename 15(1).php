4f98ff1be96da9ee445a0a600e9bba5b0be3f015
StasTopchii <87446125+StasTopchii@users.noreply.github.com>
2021-11-09 13:38:30 +0100

    Add files via upload

---
+++ 15(1).php
@@ -0,0 +1,22 @@
+<?php 
+
+srand((double)microtime()*1000000); 
+
+for ($i=1;$i<=34;$i++) 
+
+{ 
+
+$wypadlo=rand(1,2); 
+
+$wypadlo=rand(1,2); 
+if($wypadlo==1){
+    $reszka++;
+}
+if($wypadlo==2){
+    $orzel++;
+}
+} 
+echo("Reszka wypadla <b>$reszka</b> razy<br>"); 
+echo("Orzel wypadl <b>$orzel</b> raz<br>"); 
+
+?> 
